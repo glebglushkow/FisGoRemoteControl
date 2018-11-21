b48707c0e3013c6b1e600b80986961801868b46b
Gleb <glebglushkov@dreamkas.ru>
2018-11-21 23:12:47 +0300

    Initial commit

---
+++ src/main/java/sample/Controller.java
@@ -0,0 +1,70 @@
+package sample;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller {
+
+    @FXML
+    private ResourceBundle resources;
+    @FXML
+    private URL location;
+    @FXML
+    private AnchorPane mainPane;
+    @FXML
+    private JFXButton backButton;
+    @FXML
+    private JFXButton downButton;
+    @FXML
+    private JFXButton upButton;
+    @FXML
+    private JFXButton menuButton;
+    @FXML
+    private JFXButton sevenButton;
+    @FXML
+    private JFXButton eightButton;
+    @FXML
+    private JFXButton nineButton;
+    @FXML
+    private JFXButton reversalButton;
+    @FXML
+    private JFXButton goodsButton;
+    @FXML
+    private JFXButton cardButton;
+    @FXML
+    private JFXButton cashButton;
+    @FXML
+    private JFXButton enterButton;
+    @FXML
+    private JFXButton zeroButton;
+    @FXML
+    private JFXButton fourButton;
+    @FXML
+    private JFXButton fiveButton;
+    @FXML
+    private JFXButton sixButton;
+    @FXML
+    private JFXButton plusButton;
+    @FXML
+    private JFXButton oneButton;
+    @FXML
+    private JFXButton twoButton;
+    @FXML
+    private JFXButton threeButton;
+    @FXML
+    private JFXButton minusButton;
+    @FXML
+    private JFXButton commaButton;
+    @FXML
+    private JFXButton quantityButton;
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
